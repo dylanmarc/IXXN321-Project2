2f54459d3a69d6633261d84be11b8c99c606ca9c
dylanmarc <diloubilou@gmail.com>
2020-05-25 13:57:09 +1200

    Set up the folders, pages (js and css). Started on the homepage

--- ecocare/src/App.js
+++ ecocare/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './App.css';
+
+
+// import Mission from "./pages/js/Mission";
+// import Home from "./pages/js/Home";
+// import Team from "./pages/js/Team";
+// import ContactPage from "./pages/js/ContactPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {/* <Router>
+        <Nav />
+          <Route path="/mission" component={Mission} />
+          <Route path="/home" component={Home} />
+          <Route path="/team" component={Team} />
+          <Route path="/contactpage" component={ContactPage} />
+        </Router> */}
+
+        <div className="top-section">
+          <img src={require("./images/logo.svg")} alt="" className="logo"/>
+          <h1 className="title">ECOCARE</h1>
+          <h2 className="subtitle">CARETAKERS OF MOTHER EARTH</h2>
+        </div>
+
+        <div className="page-links">
+
+        </div>
+
+
+        
+      </div>
   );
 }
 
