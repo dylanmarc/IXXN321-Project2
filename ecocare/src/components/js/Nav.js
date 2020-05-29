30de973a6b4de03d26daf52082b3a8e3d661db71
dylanmarc <diloubilou@gmail.com>
2020-05-29 16:21:12 +1200

    fixed responsiveness on mission page

--- ecocare/src/components/js/Nav.js
+++ ecocare/src/components/js/Nav.js
@@ -5,7 +5,7 @@ import "../css/Nav.css"
 export default function Nav() {
     return (
         <div className="main-nav">
-            <Link to="/" className={"nav-link"}><img src={require("../../images/logo.svg")} /></Link>
+            <Link to="/" className={"nav-link"}><img src={require("../../images/logo.svg")} alt="Back to Home"/></Link>
             <Link to="/mission" className={"nav-link"}>Mission</Link>
             <Link to="/team" className={"nav-link"}>Team</Link>
             <Link to="/contactpage" className={"nav-link"}>Contact Us</Link>
