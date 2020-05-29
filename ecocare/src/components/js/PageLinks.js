15ba6697cb899201419ca357792a554909db5a10
dylanmarc <diloubilou@gmail.com>
2020-05-29 14:26:39 +1200

    finished mission page, added new background for page and nav.

--- ecocare/src/components/js/PageLinks.js
+++ ecocare/src/components/js/PageLinks.js
@@ -13,7 +13,7 @@ export default function PageLinks(props) {
             <Link to={props.path} className={"link"}>
             <div className='page-link-container'>
                 <h1>{props.title}</h1>
-                <img src={props.image} />
+                <img className="page-link-img" src={props.image} />
                 <h2>{props.blurb}</h2>
             </div>
             </Link>
